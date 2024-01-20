fe6a62cae7e249890a4b919dc7f57a6536e1b409
carlosfc02 <carlosfalconcastellano@gmail.com>
2024-01-20 21:52:54 +0000

    Class SqliteTrackLoader implements TrackLoader it's done

---
+++ src/main/java/software/ulpgc/kata4/Main.java
@@ -0,0 +1,9 @@
+package software.ulpgc.kata4;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+    }
+}
---
+++ src/main/java/software/ulpgc/kata4/SqliteTrackLoader.java
@@ -0,0 +1,48 @@
+package software.ulpgc.kata4;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqliteTrackLoader implements TrackLoader {
+    private final Connection connection;
+    private final static String SQL = "select tracks.name as track, composer, Milliseconds, title, artists.Name as artists from tracks," +
+            "albums, artists, genres where tracks.AlbumId = albums.AlbumId and albums.ArtistId = artists.ArtistId and tracks.GenreId = genre.GenreId";
+
+    public SqliteTrackLoader(Connection connection) {
+        this.connection = connection;
+    }
+
+
+    @Override
+    public List<Track> loadAll() {
+        try {
+            return load(queryAll());
+
+        }
+    }
+
+    private List<Track> load(ResultSet resultSet) throws SQLException {
+        List<Track> result = new ArrayList<>();
+        while (resultSet.next()) result.add(trackFrom(resultSet));
+        return result;
+    }
+
+    private static Track trackFrom(ResultSet resultSet) throws SQLException {
+        return new Track(
+                resultSet.getString("track"),
+                resultSet.getString("composer"),
+                resultSet.getString("title"),
+                resultSet.getString("artist"),
+                resultSet.getInt("milliseconds") / 1000
+        );
+    }
+
+    private ResultSet queryAll() throws SQLException {
+        return connection.createStatement().executeQuery(SQL);
+    }
+
+
+}
